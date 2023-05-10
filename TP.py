61ce26ae9c05b68ed0f7242fbf2624fb09a9b484
Antonius Anggito Arissaputro <antonius.anggito@ui.ac.id>
2023-05-11 02:28:15 +0700

    Add pollard rho prime factorization

--- TP.py
+++ TP.py
@@ -7,6 +7,7 @@ import gmpy2
 def num(n):
     return gmpy2.mpz(n)
 
+## Finding q
 # Miller-Rabin
 # https://stackoverflow.com/questions/17298130/working-with-large-primes-in-python
 def is_prime(n, k=10):
@@ -54,22 +55,7 @@ print('Init random q:', random_q)
 q = next_prime(random_q)
 print('q:', q)
 
-# Utility function to store prime factors of a number
-# https://www.geeksforgeeks.org/primitive-root-of-a-prime-number-n-modulo-n/
-def findPrimefactors(s, n) :
-    while (n % 2 == 0) :
-        s.add(num(2))
-        n = n // 2
-
-    for i in range(3, int(math.sqrt(n))+1, 2):
-        print(i, flush=True)
-        while (n % i == 0) :
-            s.add(num(i))
-            n = n // i
-
-    if (n > 2) :
-        s.add(n)
-
+## Finding a
 def is_primitive_root(s, num, q):
     phi = q-1
     for it in s:
@@ -77,30 +63,67 @@ def is_primitive_root(s, num, q):
             return False
     return True
 
-from sympy.ntheory import factorint
+# Pollard Rho (working)
+# https://stackoverflow.com/questions/51533621/prime-factorization-with-large-numbers-in-python
+def factors(n, b2=-1, b1=10000): # 2,3,5-wheel, then rho
+    def gcd(a,b): # euclid's algorithm
+        if b == 0: return a
+        return gcd(b, a%b)
+    def insertSorted(x, xs): # linear search
+        i, ln = 0, len(xs)
+        while i < ln and xs[i] < x: i += 1
+        xs.insert(i,x)
+        return xs
+    if -1 <= n <= 1: return [n]
+    if n < -1: return [-1] + factors(-n)
+    wheel = [1,2,2,4,2,4,2,4,6,2,6]
+    w, f, fs = 0, 2, set()
+    while f*f <= n and f < b1:
+        while n % f == 0:
+            fs.add(num(f))
+            n /= f
+        f, w = f + wheel[w], w+1
+        if w == 11: w = 3
+    if n == 1: return fs
+    fs.add(num(n))
+    return fs
+
+# # Trial Division
+# # https://www.geeksforgeeks.org/primitive-root-of-a-prime-number-n-modulo-n/
+# def findPrimefactors(s, n) :
+#     while (n % 2 == 0) :
+#         s.add(num(2))
+#         n = n // 2
+#     for i in range(3, int(math.sqrt(n))+1, 2):
+#         while (n % i == 0) :
+#             s.add(num(i))
+#             n = n // i
+#     if (n > 2) :
+#         s.add(n)
+
+# # Pollard Rho
+# # https://stackoverflow.com/questions/22827876/optimizing-a-prime-number-factorization-algorithm
+# def factor(n, c):
+#     f = lambda x: (x*x+c) % n
+#     t, h, d = 2, 2, 1
+#     while d == 1:
+#         t = f(t); h = f(f(h)); d = math.gcd(t-h, n)
+#     if d == n:
+#         return factor(n, c+1)
+#     return d
+
 def next_primitive_root(num, q):
-    # s = set()
-    # findPrimefactors(s, q-1)
-    s = set(factorint(q-1).keys()) # prime factorization using fast library
+    # findPrimefactors(s, q-1)  # trial division
+    s = factors(q-1)            # pollard rho
     print('Prime factor:', s)
 
-    start = num
     while num < q:
         print(num, flush=True)
         if is_primitive_root(s, num, q):
             return num
         num += 1
-    num = start-1
-    while num > 0:
-        print(num, flush=True)
-        if is_primitive_root(s, num, q):
-            return num
-        num -= 1
 
-# a = num(26709239175572505459)
-random_a = num(randrange(1, q-1))
-print('\nInit random a:', random_a)
-a = next_primitive_root(random_a, q)
+a = next_primitive_root(2, q)
 print('a:', a)
 
 xA = num(int(input("\nPrivate Key Alice: "))) # xA >= 10 digit < q-1
